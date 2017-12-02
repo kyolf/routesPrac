0c5e6e931fefeb6306d4fc2b9febf5b79b65241e
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 19:21:03 -0800

    added multiple controllers and hash pathing

--- prac1/Starter/app.js
+++ prac1/Starter/app.js
@@ -1,7 +1,14 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$location', '$log',
+  function($scope, $location, $log) {
     
-    
-    
+    //$location.path() gets the hash everytime it changes
+    $log.info($location.path());
+    $scope.name = 'hi';
+  }
+]);
+
+myApp.controller('secondController', ['$scope', function($scope) {
+  $scope.name = 'bye';
 }]);
