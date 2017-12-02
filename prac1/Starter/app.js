36e75ae1d289694d7b63c748117ac1a9a37036e3
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 21:40:24 -0800

    added comments of why routes are faster

--- prac1/Starter/app.js
+++ prac1/Starter/app.js
@@ -2,11 +2,18 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 //After adding ngRoute dependency, you can do $routeProvider
 //$routeProvider allows us to specify particular routes
+
+//Routes allow you to download a piece of page 
+//not the whole page.(only need to be done once)
 myApp.config(function($routeProvider) {
   $routeProvider
   .when('/', {
     templateUrl:'pages/main.html',
     controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl:'pages/second.html',
+    controller: 'secondController'
   })
   .when('/second/:num', {
     templateUrl:'pages/second.html',
@@ -25,5 +32,5 @@ myApp.controller('mainController', ['$scope', '$location', '$log',
 
 myApp.controller('secondController', ['$scope', '$routeParams', function($scope, $routeParams) {
   $scope.name = 'bye';
-  $scope.num = $routeParams.num;
+  $scope.num = $routeParams.num || 1;
 }]);
