036b3527c7236e24feb29bec31d03004e2160b97
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-12-01 21:21:59 -0800

    got routes working

--- prac1/Starter/app.js
+++ prac1/Starter/app.js
@@ -1,4 +1,18 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+//After adding ngRoute dependency, you can do $routeProvider
+//$routeProvider allows us to specify particular routes
+myApp.config(function($routeProvider) {
+  $routeProvider
+  .when('/', {
+    templateUrl:'pages/main.html',
+    controller: 'mainController'
+  })
+  .when('/second', {
+    templateUrl:'pages/second.html',
+    controller: 'secondController'
+  });
+});
 
 myApp.controller('mainController', ['$scope', '$location', '$log',
   function($scope, $location, $log) {
